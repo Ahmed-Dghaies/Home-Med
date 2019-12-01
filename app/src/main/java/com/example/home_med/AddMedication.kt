7cf5120d1063c78e988a5cb057468724389c0fdf
nwestlund <westlundn9584@my.uwstout.edu>
2019-11-30 19:20:31 -0600

    Added many new changes

--- app/src/main/java/com/example/home_med/AddMedication.kt
+++ app/src/main/java/com/example/home_med/AddMedication.kt
@@ -14,6 +14,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_add_medication.*
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.core.content.ContextCompat.getSystemService
 
 
@@ -26,19 +27,66 @@ class AddMedication : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding: FragmentAddMedicationBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_medication, container, false)
 
+        var medicationType = "Error"
+        val medicationDays = arrayListOf("Days")
+
+
         binding.saveMedicationButton.setOnClickListener { v: View ->
-            val medicationStatusCheck: Boolean = activeButton.isActivated
 
-            val medicine = m_LocalMedication(medicationName.text.toString(), medicationQty.text.toString(),
-                medicationType.text.toString(), medicationExpDate.text.toString(), medicationStatusCheck)
+            if (medicationName.text.toString().isEmpty()) {
+                Toast.makeText(context, "Please Enter a Medication Name", Toast.LENGTH_SHORT).show()
+            }
+            else if (medicationQty.text.toString().isEmpty()) {
+                Toast.makeText(context, "Please Enter a Medication Quantity", Toast.LENGTH_SHORT).show()
+            }
+            else if (medicationExpDate.text.toString().isEmpty()) {
+                Toast.makeText(context, "Please Enter a Medication Expiration Date", Toast.LENGTH_SHORT).show()
+            }
+            else if (!pillButton.isChecked && !liquidButton.isChecked) {
+                Toast.makeText(context, "Please Select a Medication Type", Toast.LENGTH_SHORT).show()
+            }
+            else {
 
-            db.collection("Medication")
-                .document(medicationName.text.toString())
-                .set(medicine)
+                if (sundayCheck.isChecked) {
+                    medicationDays.add(sundayCheck.text.toString())
+                }
+                if (mondayCheck.isChecked) {
+                    medicationDays.add(mondayCheck.text.toString())
+                }
+                if (tuesdayCheck.isChecked) {
+                    medicationDays.add(tuesdayCheck.text.toString())
+                }
+                if (wednesdayCheck.isChecked) {
+                    medicationDays.add(wednesdayCheck.text.toString())
+                }
+                if (thursdayCheck.isChecked) {
+                    medicationDays.add(thursdayCheck.text.toString())
+                }
+                if (fridayCheck.isChecked) {
+                    medicationDays.add(fridayCheck.text.toString())
+                }
+                if (saturdayCheck.isChecked) {
+                    medicationDays.add(saturdayCheck.text.toString())
+                }
 
-            val mgr = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            mgr.hideSoftInputFromWindow(medicationType.getWindowToken(), 0)
-            v.findNavController().navigate(AddMedicationDirections.actionAddMedicationToLocalMedication())
+                if (pillButton.isChecked) {
+                    medicationType = pillButton.text.toString()
+                }
+                else if (liquidButton.isChecked) {
+                    medicationType = liquidButton.text.toString()
+                }
+
+                val medicine = m_LocalMedication(medicationName.text.toString(), medicationQty.text.toString(),
+                    medicationType, medicationExpDate.text.toString(), true, medicationDays)
+
+                db.collection("Medication")
+                    .document(medicationName.text.toString())
+                    .set(medicine)
+
+                val mgr = activity!!.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                mgr.hideSoftInputFromWindow(medicationExpDate.getWindowToken(), 0)
+                v.findNavController().navigate(AddMedicationDirections.actionAddMedicationToLocalMedication())
+            }
         }
         setHasOptionsMenu(true)
         return binding.root
